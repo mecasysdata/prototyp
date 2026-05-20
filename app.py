49188d3d2e29719fd7401169d500e2cd107f28c7
mecasysdata <mecasysdata@gmail.com>
2026-05-20 22:22:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import math
 #logo - vrchná časť aplikácie -úvod
 col_logo, col_title = st.columns([1, 5])
 with col_logo:
-    try: st.image("logo.png", width=150)
+    try: st.image("logo.png", width=130)
     except: st.write("🖼️ Logo")
 with col_title:
     st.title("MEC Calculation")
